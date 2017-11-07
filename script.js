813b2ae8ee459c951fc22aca57f78c144120786a
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2017-11-07 11:03:03 -0500

    added files

---
+++ script.js
@@ -0,0 +1,74 @@
+console.log("Hello typescript again");
+/*
+let Qoutes:any = [];
+Qoutes.push({
+       "Susan B. Anthony" : "Independence is happiness.",
+   },
+   {
+       "Ernest Hemingway" : "But man is not made for defeat. A man can be destroyed but not defeated.",
+   },
+   {
+       "Franklin Roosevelt": " When you reach the end of your rope, tie a knot in it and hang on.",
+   },
+   {
+       "Heraclitus": " There is nothing permanent except change.",
+   },
+   {
+       "Indira Gandhi": " You cannot shake hands with a clenched fist..",
+   },
+   {
+       "A. P. J. Abdul Kalam": " Let us sacrifice our today so that our children can have a better tomorrow.",
+   },
+   {
+       "Albert Einstein": "You can't blame gravity for falling in love.",
+   },
+   {
+       "Leonardo da Vinci": " Learning never exhausts the mind.",
+   },
+   {
+       "Jane Austen": " There is no charm equal to tenderness of heart.",
+   },
+   {
+       "Edgar Allan Poe": " All that we see or seem is but a dream within a dream.",
+
+   },
+   {
+       "Thomas Jefferson": "  Honesty is the first chapter in the book of wisdom.",
+   },
+   {
+       "Thomas A. Edison": "I have not failed. I've just found 10,000 ways that won't work.",
+   },
+   {
+       "Milton Berle": " If opportunity doesn't knock, build a door.",
+   },
+   {
+       "Mark Twain": " The secret of getting ahead is getting started.",
+   },
+   {
+       "Socrates": " The only true wisdom is in knowing you know nothing.",
+   },
+   {
+       "Norman Vincent Peale": "Change your thoughts and you change your world.",
+   },
+   {
+       "Maya Angelou": " Try to be a rainbow in someone's cloud.",
+   });
+
+let randomQoute;
+
+function loopObject(){
+   for(let x in randomQoute){
+       return "<b>Qoute</b> : " + randomQoute[x] + "<br>" +  "<b>Author</b> : " + x ;
+       }
+  
+}
+
+function displayQoutes():any {
+   randomQoute = Qoutes[Math.floor(Math.random() * Qoutes.length)];
+  return  document.getElementById("pOne").innerHTML = loopObject();
+}
+
+
+document.getElementById("myBtn").addEventListener("click", displayQoutes);
+
+*/ 
